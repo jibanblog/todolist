f9d034aae386f74819bac9e8f731ffc60724b958
Jiban Kumar Debashis Behera <66745177+jibanblog@users.noreply.github.com>
2020-07-29 00:15:10 +0530

    Updates

---
+++ precache-manifest.98c298c85f4da88282366fdbd09d3633.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944946436eb510ad9ab6164b435a7179",
+    "url": "/todolist%20/index.html"
+  },
+  {
+    "revision": "1dbc0cf4927b78f30646",
+    "url": "/todolist%20/static/css/main.25ae3c12.chunk.css"
+  },
+  {
+    "revision": "8041f7b63e68a7ffba05",
+    "url": "/todolist%20/static/js/2.25268421.chunk.js"
+  },
+  {
+    "revision": "e04edb4657220a3d610d2b4ad704abab",
+    "url": "/todolist%20/static/js/2.25268421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dbc0cf4927b78f30646",
+    "url": "/todolist%20/static/js/main.76d1f549.chunk.js"
+  },
+  {
+    "revision": "c2a2f57253f0c6bf6fa3",
+    "url": "/todolist%20/static/js/runtime-main.3374a226.js"
+  }
+]);
